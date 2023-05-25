620c4b9ed70d2d8fd27309e9ab6e5fefd6640bbb
Millie Muteti <milliemuteti@gmail.com>
2023-05-25 15:50:24 +0300

    hard code card component

--- my-app/src/Components/Card.js
+++ my-app/src/Components/Card.js
@@ -5,7 +5,15 @@ import React from "react"
 export default function Card() {
     return (
         <section>
-            
+            <img src={require("../images/katie-zaferes.png")} alt="" />
+            <div className="card__body">
+                <span>
+                <img src={require("../images/star.png")} alt="" />
+                5.0 (6) .USA
+                </span> 
+                <p>Life lessons with Katie Zaferes</p>
+                <p>From $136 / person</p>
+            </div>
         </section>
     )
 }
