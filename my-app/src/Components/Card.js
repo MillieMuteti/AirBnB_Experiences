64c6983742fe3fa2d4c0a1cb1b0bf924565fa868
Millie Muteti <milliemuteti@gmail.com>
2023-05-25 16:16:43 +0300

    style card

--- my-app/src/Components/Card.js
+++ my-app/src/Components/Card.js
@@ -4,16 +4,20 @@ import React from "react"
 
 export default function Card() {
     return (
-        <section>
-            <img src={require("../images/katie-zaferes.png")} alt="" />
+        <div className="card">
+            <img src={require("../images/katie-zaferes.png")} alt="" className="card__image" />
             <div className="card__body">
-                <span>
-                <img src={require("../images/star.png")} alt="" />
-                5.0 (6) .USA
-                </span> 
+                <div className="card__stats">
+                <img src={require("../images/star.png")} alt="" className="card__star"  />
+                <span>5.0 </span>
+                <span className="gray">(6) .</span>
+                <span className="gray">USA</span>
+                 
+               
+                </div> 
                 <p>Life lessons with Katie Zaferes</p>
-                <p>From $136 / person</p>
+                <p><span className="bold">From $136</span> / person</p>
             </div>
-        </section>
+        </div>
     )
 }
