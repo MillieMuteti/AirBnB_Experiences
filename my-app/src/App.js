945e7a59427cb5483bb6cbb5d4901763a2d7406a
Millie Muteti <milliemuteti@gmail.com>
2023-05-26 13:04:05 +0300

    add props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,14 @@ export default function App() {
         <div className="main">
            <Navbar />
             {/* <Hero /> */}
-            <Card />
+            <Card 
+            img="(../images/katie-zaferes.png)"
+            rating={5.0}
+            reviewCount="(6)"
+            country="USA"
+            title="Life Lessons with Katie Zaferes"
+            price={136}
+            />
         </div>
 
     )
--- my-app/src/Components/Card.js
+++ my-app/src/Components/Card.js
@@ -2,21 +2,21 @@ import React from "react"
 
 
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={require("../images/katie-zaferes.png")} alt="" className="card__image" />
+            <img src= {props.img}alt="" className="card__image" />
             <div className="card__body">
                 <div className="card__stats">
                 <img src={require("../images/star.png")} alt="" className="card__star"  />
-                <span>5.0 </span>
-                <span className="gray">(6) .</span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">{props.reviewCount} .</span>
+                <span className="gray">{props.country}</span>
                  
                
                 </div> 
-                <p>Life lessons with Katie Zaferes</p>
-                <p><span className="bold">From $136</span> / person</p>
+                <p>{props.title}</p>
+                <p><span className="bold">From ${props.price}</span> / person</p>
             </div>
         </div>
     )
